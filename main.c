dbab8cf54840c5af77bc859ba5334651ab63cf61
Benglad24 <119924211+Benglad24@users.noreply.github.com>
2023-10-28 14:38:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,47 @@
+#include <stdio.h>      // printf, perror
+#include <sys/types.h>  // pid_t
+#include <unistd.h>     // fork
+
+#define MAX_IMAGES 100      
+
+
+void ListeimageBank(FILE searim, char* images[], int* numImages) {
+   char imagePath[MAX_PATH_LENGTH];
+   int i = 0;
+   while (i < MAX_IMAGES && fgets(imagePath, MAX_PATH_LENGTH, searim) != NULL) {
+      size_t len = strlen(imagePath);
+      if (imagePath[len - 1] == '\n') {//enleve saut de ligne  en /0
+         imagePath[len - 1] = '\0';
+      }
+      images[i] = imagePath;
+      i++;
+   }
+   *numImages = i;
+}
+
+
 int main(int argc, char* argv[]) {
-   return 0;
+   FILE searim = argv[1];
+   char* images[MAX_IMAGES];
+   int numImages = 0;
+   ListeimageBank( searim,images, &numImages);
+
+   pid_t child1 = fork();
+   if (child1 > 0) {  // Père
+    printf("Je suis le père.\n");
+    } else if (child1 == 0) {  // Fils
+    printf("Je suis le fils.\n");
+   } else {             // Erreur
+    perror("fork()");  // Affiche sur stderr "fork(): <description de l'erreur>"
+    return 1;
+   }
+   pid_t child2 = fork();
+   if (child2 > 0) {  // Père
+    printf("Je suis le père.\n");
+   } else if (child2 == 0) {  // Fils
+    printf("Je suis le fils.\n");
+   } else {             // Erreur
+    perror("fork()");  // Affiche sur stderr "fork(): <description de l'erreur>"
+    return 1;
+   }
 }
