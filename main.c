41d3dc6b72a88b161aa006c9854b46929ec91776
TBKNbr1 <stephane.badi.budu@ulb.be>
2023-11-03 15:48:29 +0100

    3.2 fait

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>      // printf, perror
 #include <sys/types.h>  // pid_t
 #include <unistd.h>     // fork
+#include <string.h>
+
+
 
 int main(int argc, char* argv[]) {
-   return 0;
+   char* imgPaths[2];
+   int c=0;
+   for (int i=1; i < argc; i++){
+      if (strcmp(argv[i], "-v") != 0 && strlen(argv[i]) < 1000){
+         imgPaths[c] = argv[i];
+         if (argv[i][-1] != '\0'){
+            imgPaths[c][strlen(argv[i])] = '\0';
+         }
+         c++;
+      }
+   }
 }
