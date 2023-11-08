0b03604fd3e3967163909f214e5d5beb33700ee2
TBKNbr1 <118356262+TBKNbr1@users.noreply.github.com>
2023-11-08 17:51:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>     // fork
 #include <sys/wait.h>   // wait
 #include <string.h>     // strcmp & strlen
+#include <stdlib.h>     // exit
 
 #define READ 0
 #define WRITE 1
@@ -12,15 +13,14 @@ int main(int argc, char* argv[]) {
    imgPaths[0] = argv[0];
    if (argc == 2 && strcmp(argv[0], "-v") != 0 && strlen(argv[1]) < 1000){
       imgPaths[0] = argv[1];
-      if (argv[1][-1] != '\0'){
-         imgPaths[0][strlen(argv[1])] = '\0';
-      }
    }
    else {
       return 0;
    }
    
-   char buffer[1024][100]; int taille = 0;
+   char buffer[1024][100];
+
+   int taille = 0;
    while (fgets(buffer[taille], sizeof(buffer[taille]), stdin) != NULL){
       taille++;
    } 
@@ -29,72 +29,6 @@ int main(int argc, char* argv[]) {
       printf("No similar image found (no comparison could be performed successfully).\n");
       return 1;
    }
-   
-      for (int j = 0; j<taille; j++) {
-      printf("Fichier n°%d : %s", j+1, buffer[j]);
-   }
-
-   int pipe1[2], pipe2[2];
-
-   pid_t child1, child2;
-
-   if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
-      perror("Erreur");
-      exit(1); // Échec
-   }
-
-   child1 = fork();
-   if (child1 == -1) {
-      perror("Erreur");
-      exit(1); // Échec
-   }
-
-   if (child1 == 0) {
-      close(pipe1[WRITE]); 
-      char chemin_recu[100];
-      read(pipe1[READ], chemin_recu, sizeof(chemin_recu));
-      printf("enfant 1 pid(%d) chemin : %s", getpid(), chemin_recu);
-      close(pipe1[READ]); // Fermez l'extrémité de lecture du premier tube
-      exit(0); // Succès
-   }
-
-   child2 = fork();
-   if (child2 == -1) {
-      perror("Erreur");
-      exit(1); // Échec
-   }
-
-   if (child2 == 0) {
-      close(pipe2[WRITE]); 
-      char chemin_recu[100];
-      read(pipe2[READ], chemin_recu, sizeof(chemin_recu));
-      printf("enfant 2  pid (%d) chemin : %s", getpid(), chemin_recu);
-      close(pipe2[READ]); 
-      exit(0); // Succès
-   }
-
-
-   for (int i = 0; i < taille / 2; i++) {
-      
-      char chemin1[] = buffer[i];
-      char chemin2[]= buffer[taille - i - 1];
-      
-      
-      // Code du processus parent
-      close(pipe1[READ]);
-      close(pipe2[READ]);
-
-      write(pipe1[WRITE], chemin1, strlen(chemin1) + 1);
-      write(pipe2[WRITE], chemin2, strlen(chemin2) + 1);
-      
-      close(pipe1[WRITE]); 
-      close(pipe2[WRITE]);
-
-      wait(NULL); // Attendez la fin des processus enfants
-      wait(NULL);
-      }
-
-
 
    return 0;
-   }
+}
