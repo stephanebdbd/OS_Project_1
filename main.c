4decb94824191d1e37a50f75381cb768dc9e0fc9
TBKNbr1 <118356262+TBKNbr1@users.noreply.github.com>
2023-11-09 15:39:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,10 +9,10 @@
 #define WRITE 1
 
 int main(int argc, char* argv[]) {
-   char* imgPaths[1];
-   imgPaths[0] = argv[0];
+   //char* imgPaths[1];
+   //imgPaths[0] = argv[0];
    if (argc == 2 && strcmp(argv[0], "-v") != 0 && strlen(argv[1]) < 1000){
-      imgPaths[0] = argv[1];
+   //   imgPaths[0] = argv[1];
    }
    else {
       return 0;
@@ -29,6 +29,72 @@ int main(int argc, char* argv[]) {
       printf("No similar image found (no comparison could be performed successfully).\n");
       return 1;
    }
+
+   int pipe1[2], pipe2[2];
+   pid_t child1, child2;
+
+   if (pipe(pipe1) == -1 || pipe(pipe2) == -1)
+   {
+      perror("Erreur");
+      exit(1);
+   }
+
+   child1 = fork();
+   if (child1 == -1){
+      perror("fork()");
+      exit(1);
+   }
+   
+
+   if (child1 == 0){
+      close(pipe1[WRITE]);
+      close(pipe2[READ]);
+      close(pipe2[WRITE]);
+      char chemin_recu[100];
+      while (read(pipe1[READ], &chemin_recu, sizeof(chemin_recu))){
+         printf("enfant 1 pid (%d) chemin : %s\n", getpid(), chemin_recu);
+      }
+      close(pipe1[READ]);
+      exit(0);
+   }
+
+   else {
+   child2 = fork();
+   if (child2 == -1){
+      perror("fork()");
+      exit(1);
+   }
+   if (child2 == 0) {
+      close(pipe2[WRITE]);
+      close(pipe1[READ]);
+      close(pipe1[WRITE]);
+      char chemin_recu[100];
+      while (read(pipe2[READ], &chemin_recu, sizeof(chemin_recu))){
+         printf("enfant 2 pid (%d) chemin : %s\n", getpid(), chemin_recu);
+      }
+      close(pipe2[READ]);
+      exit(0);
+      }
+   }
+   
+   close(pipe1[READ]);
+   close(pipe2[READ]);
+   
+
+   for (int i=0; i < taille; i++) {
+      if (i % 2 == 0){
+         write(pipe1[WRITE], &buffer[i], sizeof(buffer[i]));
+      }
+      else {
+         write(pipe2[WRITE], &buffer[i], sizeof(buffer[i]));
+      }
+   }
+   
+   close(pipe1[WRITE]);
+   close(pipe2[WRITE]);
+
+   wait(NULL);
+   wait(NULL);
 
    return 0;
 }
